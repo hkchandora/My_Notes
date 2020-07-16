cd154e33d2341af85721de08d8aa3620608bb2ac
Himanshu Kumawat <chandorahimanshu@gmail.com>
2020-07-16 11:19:42 +0530

    Implement Bottom Navigation

--- app/src/main/java/com/himanshu/mynotes/SplashScreen.java
+++ app/src/main/java/com/himanshu/mynotes/SplashScreen.java
@@ -32,7 +32,7 @@ public class SplashScreen extends AppCompatActivity {
                     } catch (Exception e) {
                         e.printStackTrace();
                     } finally {
-                        startActivity(new Intent(getApplicationContext(), DashBoardActivity.class));
+                        startActivity(new Intent(getApplicationContext(), MainDashBoardActivity.class));
                         finish();
                     }
                 }
