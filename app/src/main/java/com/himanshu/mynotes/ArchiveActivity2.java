750a81bf55e81be35f002607de5e2421b4ba346b
Himanshu Kumawat <chandorahimanshu@gmail.com>
2020-07-17 09:27:21 +0530

    Profile Module

---
+++ app/src/main/java/com/himanshu/mynotes/ArchiveActivity2.java
@@ -0,0 +1,14 @@
+package com.himanshu.mynotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArchiveActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_archive2);
+    }
+}
