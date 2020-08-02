82015499feed04e14cc4c6b969ec95d475df3f69
Himanshu Kumawat <chandorahimanshu@gmail.com>
2020-08-02 15:17:47 +0530

    Update APP Dialog

--- app/src/main/java/com/himanshu/mynotes/MainDashBoardActivity.java
+++ app/src/main/java/com/himanshu/mynotes/MainDashBoardActivity.java
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.app.ActivityOptions;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.himanshu.mynotes.fragment.DashBoardFragment;
 import com.himanshu.mynotes.fragment.ProfileFragment;
 import com.luseen.spacenavigation.SpaceItem;
@@ -26,6 +37,8 @@ public class MainDashBoardActivity extends AppCompatActivity {
     private Fragment profileFragment;
     private Fragment dashboardFragment;
     private Fragment currentFragment;
+    private int version;
+    private Boolean isMandatory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +85,9 @@ public class MainDashBoardActivity extends AppCompatActivity {
             public void onItemReselected(int itemIndex, String itemName) {
             }
         });
+
+
+        checkUpdateAvailable();
     }
 
     @Override
@@ -80,7 +96,7 @@ public class MainDashBoardActivity extends AppCompatActivity {
             finish();
             return;
         }
-        if(currentFragment instanceof ProfileFragment){
+        if (currentFragment instanceof ProfileFragment) {
             currentFragment = dashboardFragment;
             spaceNavigationView.changeCurrentItem(0);
             getSupportFragmentManager().beginTransaction()
@@ -96,5 +112,74 @@ public class MainDashBoardActivity extends AppCompatActivity {
     public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
         super.onSaveInstanceState(outState, outPersistentState);
         spaceNavigationView.onSaveInstanceState(outState);
+    }
+
+    public void checkUpdateAvailable() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("appInfo");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    version = Integer.parseInt(snapshot.child("version").getValue() + "");
+                    isMandatory = Boolean.parseBoolean(snapshot.child("isMandatory").getValue() + "");
+                    if (version > 1) {
+                        updateDialogShow(isMandatory);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void updateDialogShow(Boolean isMandatory) {
+
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_update_app);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(false);
+
+        Button laterBtn = dialog.findViewById(R.id.dialog_update_app_later_btn);
+        Button exitBtn = dialog.findViewById(R.id.dialog_update_app_exit_btn);
+        Button updateBtn = dialog.findViewById(R.id.dialog_update_app_update_btn);
+
+        if (isMandatory) {
+            exitBtn.setVisibility(View.VISIBLE);
+            laterBtn.setVisibility(View.GONE);
+        }
+
+        laterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("market://details?id=" + MainDashBoardActivity.this.getPackageName())));
+                } catch (android.content.ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" + MainDashBoardActivity.this.getPackageName())));
+                }
+            }
+        });
+        dialog.show();
     }
 }
